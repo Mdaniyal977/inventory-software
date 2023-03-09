8220ba776c3e407c43ad9ea882b64aa3aad83b03
daniyal <daniyalmuhammad977@gmail.com>
2023-03-09 12:15:28 +0500

    new

--- app.js
+++ app.js
@@ -7,6 +7,46 @@ function printDiv() {
   printWindow.document.close();
   printWindow.print();
 }
+
+const container = document.querySelector('.container');
+const divs = container.querySelectorAll('div');
+const numDivs = divs.length;
+const divsPerPage = 6; // Set the number of divs to show per page
+let currentPage = 1;
+
+function showDivs(page) {
+  const startIndex = (page - 1) * divsPerPage;
+  const endIndex = startIndex + divsPerPage;
+  
+  for (let i = 0; i < numDivs; i++) {
+    if (i >= startIndex && i < endIndex) {
+      divs[i].classList.remove('hidden');
+    } else {
+      divs[i].classList.add('hidden');
+    }
+  }
+}
+
+showDivs(currentPage);
+
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
+prevButton.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    showDivs(currentPage);
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentPage < Math.ceil(numDivs / divsPerPage)) {
+    currentPage++;
+    showDivs(currentPage);
+  }
+});
+
+
   var quantity =document.getElementById("horn-quantity").innerText
   var price=document.getElementById("horn-price")
   var popup=document.getElementsByClassName(".popuphorn")
