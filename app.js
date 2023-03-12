67bdeba0b37f2880ea1b3c300adde8a8018aa39d
daniyal <daniyalmuhammad977@gmail.com>
2023-03-13 01:01:00 +0500

    new

--- app.js
+++ app.js
@@ -8,50 +8,82 @@ function printDiv() {
   printWindow.print();
 }
 
-const container = document.querySelector('.container');
-const divs = container.querySelectorAll('div');
-const numDivs = divs.length;
-const divsPerPage = 6; // Set the number of divs to show per page
-let currentPage = 1;
+// Get all the divs with class "myDiv" and store them in an array
+var divs = document.querySelectorAll('.myDiv');
 
-function showDivs(page) {
-  const startIndex = (page - 1) * divsPerPage;
-  const endIndex = startIndex + divsPerPage;
-  
-  for (let i = 0; i < numDivs; i++) {
-    if (i >= startIndex && i < endIndex) {
-      divs[i].classList.remove('hidden');
+// Get the search input and button
+var searchInput = document.getElementById('search-input');
+var searchBtn = document.getElementById('search-btn');
+var clearBtn = document.getElementById('clear-btn');
+
+// Get the pagination buttons
+var prevBtn = document.getElementById('prev-btn');
+var nextBtn = document.getElementById('next-btn');
+
+// Set the default page number and number of items per page
+var currentPage = 1;
+var itemsPerPage = 8;
+
+// Set event listeners for the search and clear buttons
+searchBtn.addEventListener('click', searchDivs);
+clearBtn.addEventListener('click', clearSearch);
+
+// Set event listeners for the pagination buttons
+prevBtn.addEventListener('click', prevPage);
+nextBtn.addEventListener('click', nextPage);
+
+// Function to search the divs
+function searchDivs() {
+  var searchTerm = searchInput.value.toLowerCase();
+  divs.forEach(function(div) {
+    var text = div.textContent.toLowerCase();
+    if (text.includes(searchTerm)) {
+      div.style.display = 'block';
     } else {
-      divs[i].classList.add('hidden');
+      div.style.display = 'none';
     }
+  });
+  currentPage = 1;
+}
+
+// Function to clear the search and show all divs
+function clearSearch() {
+  searchInput.value = '';
+  divs.forEach(function(div) {
+    div.style.display = 'block';
+  });
+  currentPage = 1;
+  showDivs();
+}
+
+// Function to go to the previous page
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    showDivs();
   }
 }
 
-showDivs(currentPage);
-
-const prevButton = document.querySelector('#prev');
-const nextButton = document.querySelector('#next');
-
-prevButton.addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    showDivs(currentPage);
-  }
-});
-
-nextButton.addEventListener('click', () => {
-  if (currentPage < Math.ceil(numDivs / divsPerPage)) {
+// Function to go to the next page
+function nextPage() {
+  if (currentPage < Math.ceil(divs.length / itemsPerPage)) {
     currentPage++;
-    showDivs(currentPage);
+    showDivs();
   }
-});
+}
 
+// Function to show the divs for the current page
+function showDivs() {
+  var startIndex = (currentPage - 1) * itemsPerPage;
+  var endIndex = startIndex + itemsPerPage;
+  divs.forEach(function(div, index) {
+    if (startIndex <= index && index < endIndex) {
+      div.style.display = 'block';
+    } else {
+      div.style.display = 'none';
+    }
+  });
+}
 
-  var quantity =document.getElementById("horn-quantity").innerText
-  var price=document.getElementById("horn-price")
-  var popup=document.getElementsByClassName(".popuphorn")
-  var l=document.getElementsByClassName(".newvalueofhorn").value
-
-  console.log(quantity)
-  console.log(l)
-  console.log(popup)
+// Show the first 10 divs on page load
+showDivs();
