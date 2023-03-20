133e4bc9dd43be086e79febaaee02763a334945c
daniyal <daniyalmuhammad977@gmail.com>
2023-03-20 11:40:39 +0500

    enw

--- public/app.js
+++ public/app.js
@@ -1,168 +1,134 @@
 function printDiv() {
-//   var divContents = document.getElementsById("myDiv").innerHTML;
-  var divContents = document.getElementById("myDiv").innerHTML
-  var printContents = "<html><head><title></title></head><body>" + divContents + "</body></html>";
-  var printWindow = window.open('', '', 'height=600,width=800');
-  printWindow.document.write(printContents);
-  printWindow.document.close();
-  printWindow.print();
-}
-
-// Get all the divs with class "myDiv" and store them in an array
-var divs = document.querySelectorAll('.myDiv');
-
-// Get the search input and button
-var searchInput = document.getElementById('search-input');
-var searchBtn = document.getElementById('search-btn');
-var clearBtn = document.getElementById('clear-btn');
-
-// Get the pagination buttons
-var prevBtn = document.getElementById('prev-btn');
-var nextBtn = document.getElementById('next-btn');
-
-// Set the default page number and number of items per page
-var currentPage = 1;
-var itemsPerPage = 8;
-
-// Set event listeners for the search and clear buttons
-searchBtn.addEventListener('click', searchDivs);
-clearBtn.addEventListener('click', clearSearch);
-
-// Set event listeners for the pagination buttons
-prevBtn.addEventListener('click', prevPage);
-nextBtn.addEventListener('click', nextPage);
-
-// Function to search the divs
-function searchDivs() {
-  var searchTerm = searchInput.value.toLowerCase();
-  divs.forEach(function(div) {
-    var text = div.textContent.toLowerCase();
-    if (text.includes(searchTerm)) {
+  //   var divContents = document.getElementsById("myDiv").innerHTML;
+    var divContents = document.getElementById("myDiv").innerHTML
+    var printContents = "<html><head><title></title></head><body>" + divContents + "</body></html>";
+    var printWindow = window.open('', '', 'height=600,width=800');
+    printWindow.document.write(printContents);
+    printWindow.document.close();
+    printWindow.print();
+  }
+  
+  // Get all the divs with class "myDiv" and store them in an array
+  var divs = document.querySelectorAll('.myDiv');
+  
+  // Get the search input and button
+  var searchInput = document.getElementById('search-input');
+  var searchBtn = document.getElementById('search-btn');
+  var clearBtn = document.getElementById('clear-btn');
+  
+  // Get the pagination buttons
+  var prevBtn = document.getElementById('prev-btn');
+  var nextBtn = document.getElementById('next-btn');
+  
+  // Set the default page number and number of items per page
+  var currentPage = 1;
+  var itemsPerPage = 8;
+  
+  // Set event listeners for the search and clear buttons
+  searchBtn.addEventListener('click', searchDivs);
+  clearBtn.addEventListener('click', clearSearch);
+  
+  // Set event listeners for the pagination buttons
+  prevBtn.addEventListener('click', prevPage);
+  nextBtn.addEventListener('click', nextPage);
+  
+  // Function to search the divs
+  function searchDivs() {
+    var searchTerm = searchInput.value.toLowerCase();
+    divs.forEach(function(div) {
+      var text = div.textContent.toLowerCase();
+      if (text.includes(searchTerm)) {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+    currentPage = 1;
+  }
+  
+  // Function to clear the search and show all divs
+  function clearSearch() {
+    searchInput.value = '';
+    divs.forEach(function(div) {
       div.style.display = 'block';
-    } else {
-      div.style.display = 'none';
+    });
+    currentPage = 1;
+    showDivs();
+  }
+  
+  // Function to go to the previous page
+  function prevPage() {
+    if (currentPage > 1) {
+      currentPage--;
+      showDivs();
     }
-  });
-  currentPage = 1;
-}
-
-// Function to clear the search and show all divs
-function clearSearch() {
-  searchInput.value = '';
-  divs.forEach(function(div) {
-    div.style.display = 'block';
-  });
-  currentPage = 1;
+  }
+  
+  // Function to go to the next page
+  function nextPage() {
+    if (currentPage < Math.ceil(divs.length / itemsPerPage)) {
+      currentPage++;
+      showDivs();
+    }
+  }
+  
+  // Function to show the divs for the current page
+  function showDivs() {
+    var startIndex = (currentPage - 1) * itemsPerPage;
+    var endIndex = startIndex + itemsPerPage;
+    divs.forEach(function(div, index) {
+      if (startIndex <= index && index < endIndex) {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+  }
+  
+  // Show the first 10 divs on page load
   showDivs();
-}
-
-// Function to go to the previous page
-function prevPage() {
-  if (currentPage > 1) {
-    currentPage--;
-    showDivs();
-  }
-}
-
-// Function to go to the next page
-function nextPage() {
-  if (currentPage < Math.ceil(divs.length / itemsPerPage)) {
-    currentPage++;
-    showDivs();
-  }
-}
-
-// Function to show the divs for the current page
-function showDivs() {
-  var startIndex = (currentPage - 1) * itemsPerPage;
-  var endIndex = startIndex + itemsPerPage;
-  divs.forEach(function(div, index) {
-    if (startIndex <= index && index < endIndex) {
-      div.style.display = 'block';
-    } else {
-      div.style.display = 'none';
-    }
+  
+  
+  
+  
+  
+  
+  // Get the current date and time
+  const now = new Date();
+  
+  // Get the date and time strings
+  const dateString = now.toLocaleDateString();
+  const timeString = now.toLocaleTimeString();
+  
+  // Get the date and time span elements
+  const dateSpan = document.querySelector('#date');
+  const timeSpan = document.querySelector('#time');
+  
+  // Set the text content of the span elements to the date and time strings
+  dateSpan.textContent = dateString;
+  timeSpan.textContent = timeString;
+  
+  // get all update buttons
+  const updateButtons = document.querySelectorAll(".update-button");
+  
+  // add event listener to each update button
+  updateButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      // get the corresponding div element
+      const div = event.target.parentElement;
+  
+      // prompt user for stock and price
+      const stock = prompt("Enter new stock:");
+      const price = prompt("Enter new price:");
+  
+      // update the stock and price spans
+      div.querySelector("span[id$='-stock']").textContent = stock;
+      div.querySelector("span[id$='-price']").textContent = price;
+  
+      // display confirmation message
+      alert("Stock and price updated!");
+    });
   });
-}
-
-// Show the first 10 divs on page load
-showDivs();
 
 
 
-
-
-
-// Get the current date and time
-const now = new Date();
-
-// Get the date and time strings
-const dateString = now.toLocaleDateString();
-const timeString = now.toLocaleTimeString();
-
-// Get the date and time span elements
-const dateSpan = document.querySelector('#date');
-const timeSpan = document.querySelector('#time');
-
-// Set the text content of the span elements to the date and time strings
-dateSpan.textContent = dateString;
-timeSpan.textContent = timeString;
-
-
-// Define an empty object to store stock and price values for each product
-let products = {};
-
-// Retrieve existing values from localStorage, if any
-const storedData = JSON.parse(localStorage.getItem("productData"));
-if (storedData) {
-  products = storedData;
-  // Loop through each product element on the page and update its stock and price values
-  const productElems = document.querySelectorAll(".product-box-1");
-  productElems.forEach((product) => {
-    const id = product.id;
-    if (id in products) {
-      const quantitySpan = product.querySelector(`#${id}-quantity`);
-      quantitySpan.textContent = products[id].stock;
-      const priceSpan = product.querySelector(`#${id}-price`);
-      priceSpan.textContent = products[id].price;
-    }
-  });
-}
-
-// get all update buttons
-const updateButtons = document.querySelectorAll(".update-button");
-
-// add event listener to each update button
-updateButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    // get the corresponding div element
-    const div = event.target.parentElement;
-
-    // prompt user for stock and price
-    const stock = prompt("Enter new stock:");
-    const price = prompt("Enter new price:");
-
-    // update the stock and price spans
-    div.querySelector("span[id$='-quantity']").textContent = stock;
-    div.querySelector("span[id$='-price']").textContent = price;
-
-    // display confirmation message
-    alert("Stock and price updated!");
-  });
-});
-
-
-
-
-// get the current product data from the page
-const productElems = document.querySelectorAll(".product-box-1");
-const productData = {};
-productElems.forEach((product) => {
-  const id = product.id;
-  const quantitySpan = product.querySelector(`#${id}-quantity`);
-  const priceSpan = product.querySelector(`#${id}-price`);
-  productData[id] = {stock: quantitySpan.textContent, price: priceSpan.textContent};
-});
-
-// store the product data in local storage
-localStorage.setItem("productData", JSON.stringify(productData));
